4981fd49474b4081662af84954725263e124cae9
shubh <shubham.sahu@opstree.com>
2024-12-19 01:00:01 +0530

    adding python code 1 and 2 and workflow file with dropdown menu

---
+++ Python_Code-app1.py
@@ -0,0 +1,4 @@
+# app1.py
+print("Executing Python Code 1...")
+result = sum([1, 2, 3, 4, 5])
+print(f"Result of Code 1: {result}")
---
+++ Python_Code-app2.py
@@ -0,0 +1,5 @@
+# app2.py
+print("Executing Python Code 2...")
+result = [x**2 for x in range(5)]
+print(f"Result of Code 2: {result}")
+
